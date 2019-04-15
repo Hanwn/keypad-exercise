66baeaa84fb8f7f4aee78887bdbb3edbd01f0539
Hanwn <1934611881@qq.com>
2019-04-15 15:49:38 +0800

    完善自定义功能

--- calculate.py
+++ calculate.py
@@ -2,12 +2,14 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import sys
 import random
+import default
+import matplotlib as plt
 
 
 class Calculate(QMainWindow,QLineEdit,QWidget):
     def __init__(self):
         super().__init__()
-        self.result = self.get_result()
+        self.flag = 0
         self.timer = QBasicTimer()
         self.line1 = QTextEdit()
         self.line1.setFocusPolicy(Qt.NoFocus)
@@ -21,19 +23,31 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
 
         self.btn = QPushButton('开始',self)
         self.restart()
+        self.result = self.get_result(0)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.Dialog)
         self.palette1 = QPalette()
         self.palette1.setColor(self.backgroundRole(), QColor(242,156,177))   # 设置背景颜色
         self.ui_init()
 
-    def restart(self):
-        self.count = 1
-        self.rcount = 0
-        self.ecount = 0
-        self.step = 0
-        self.timer.stop()
-        self.btn.setText('开始')
 
+
+        tb = QToolButton(self)
+        tb.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        # tb.setArrowType(Qt.DownArrow)
+        tb.setToolTip('选择适合你的练习方式')
+        tb.setPopupMode(QToolButton.MenuButtonPopup)
+        tb.setText('练习方式')
+        tb.setAutoRaise(True)
+
+        menu = QMenu(self)
+        self.selfAct = QAction('自定义练习', self)
+        self.defaultAct = QAction('默认练习', self)
+        menu.addAction(self.selfAct)
+        menu.addAction(self.defaultAct)
+        menu.addSeparator()
+        tb.setMenu(menu)
+        self.selfAct.triggered.connect(self.on_click)
+        self.defaultAct.triggered.connect(self.on_click)
 
     def ui_init(self):
         self.resize(600,200)
@@ -83,6 +97,44 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
         self.setPalette(self.palette1)
         self.setCentralWidget(widget)
 
+    def on_click(self):
+        if self.sender() == self.selfAct:
+            self.d = default.Demo()
+            self.flag = 1
+            self.d.dbtn.clicked.connect(self.get_text)
+            self.d.dbtn1.clicked.connect(self.demo_close)
+
+        elif self.sender() == self.defaultAct:
+            self.flag = 0
+            self.restart()
+    
+    def demo_close(self):
+        self.d.close()
+
+    def get_text(self):
+        self.ncount = int(self.d.sp1.text())
+        self.matrial = self.d.line.text()
+        # print(self.matrial)
+        self.result = self.get_result(1)
+        self.len_time = int(self.d.sp2.text()) * 600
+        # print(self.result)
+        self.restart()
+        self.d.close()
+
+
+    def restart(self):
+        self.count = 1
+        self.rcount = 0
+        self.ecount = 0
+        if self.flag == 1:
+            self.result = self.get_result(1)
+        else:
+            self.result = self.get_result(0)
+            self.len_time = 6000
+            self.ncount = 300
+        self.step = 0
+        self.timer.stop()
+        self.btn.setText('开始')
 
 
     def center(self):
@@ -95,28 +147,50 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
     def onTextChanged(self,text):
         if len(text) % 7 == 6:
             inputnumber = self.line2.text()
-            if(inputnumber == self.result[self.count - 1]):
-                self.rcount = self.rcount + 1
-                # print("right:{}".format(self.rcount))
+            if inputnumber == '':
+                pass
             else:
-                self.ecount = self.ecount + 1
-                # print("error:{}".format(self.ecount))
-            self.line2.setText('')
-            self.line1.setText("<font size=%s><B>%s</B></font>" %("15", "{}".format(self.result[self.count])))
-            self.count = self.count + 1
+                try:
+                    if(inputnumber == self.result[self.count - 1]):
+                        self.rcount = self.rcount + 1
+                        # print("right:{}".format(self.rcount))
+                    else:
+                        print('ok')
+                        print(inputnumber)
+                        self.ecount = self.ecount + 1
+                        # print("error:{}".format(self.ecount))
+                except Exception as e:
+                    # print(e)
+                    pass
+                self.line2.setText('')
+                try:
+                    self.line1.setText("<font size=%s><B>%s</B></font>" %("15", "{}".format(self.result[self.count])))
+                except Exception as e:
+                    # print(e)
+                    pass
+                self.count = self.count + 1
 
-    def get_result(self):
+    def get_result(self,flag):
         result = []
-        s = ""
-        for j in range(0,300):
-            for i in range(0,6):
-                s = s +str(random.randint(0,9))
-            result.append(s)
-            s = ""
+        if flag == 0:
+            for j in range(0,300):
+                s = ""
+                for i in range(0,6):
+                    s = s +str(random.randint(0,9))
+                result.append(s)
+        else:
+            for i in range(0,self.ncount):
+                s = ''
+                for j in range(0,6):
+                    s = s + str(random.choice(self.matrial))
+                result.append(s)
         return result
 
     def onstart(self):
-        self.line1.setText("<font size=%s><B>%s</B></font>" %("15", "{}".format(self.result[self.count - 1])))
+        try:
+            self.line1.setText("<font size=%s><B>%s</B></font>" %("15", "{}".format(self.result[self.count - 1])))
+        except Exception as e:
+            pass
         self.line2.textChanged.connect(self.onTextChanged)
         self.line2.setValidator(QRegExpValidator(QRegExp("(\\d+)")))
 
@@ -124,11 +198,11 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
             self.timer.stop()
             self.btn.setText('开始')
         else:
-            self.timer.start(100,self)
+            self.timer.start(self.len_time,self)
             self.btn.setText('停止')
 
     def timerEvent(self,event):
-        if(self.step >= 100):
+        if(self.step >= self.len_time or self.ncount<self.count):
             self.timer.stop()
             self.btn.setText('完成')
             self.information()
@@ -137,9 +211,7 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
         self.pbar.setValue(self.step)
 
     def quitClicked(self):
-        reply = QMessageBox.question(self, 'Warning',
-                                           'Are you sure to quit?', QMessageBox.Yes,
-                                           QMessageBox.No)
+        reply = QMessageBox.question(self, 'Warning','Are you sure to quit?', QMessageBox.Yes,QMessageBox.No)
         if reply == QMessageBox.Yes:
             quit()
 
@@ -147,9 +219,8 @@ class Calculate(QMainWindow,QLineEdit,QWidget):
         pass
 
     def information(self):
-        #QMessageBox.information(self,'information',self.tr("正确：{},错误：{},正确率：{}".format(self.rcount,self.ecount,self.rcount/300)))
-        #print("正确：{},错误：{},正确率：{}".format(self.rcount,self.ecount,self.rcount/300))
-        reply = QMessageBox.information(self,'你的成绩','正确：{},错误：{},正确率：{}'.format(self.rcount,self.ecount,self.rcount/300))
+        reply = QMessageBox.information(self,'你的成绩','正确：{},错误：{},正确率：{}%'.format(self.rcount,self.ecount,self.rcount/self.ncount * 100))
+
 
 def main():
     app = QApplication(sys.argv)
